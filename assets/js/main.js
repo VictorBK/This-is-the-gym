3307fc0aced3a088f5c06f4f661d042e32a35ad6
VictorBK <victorbrian4@gmail.com>
2023-06-18 11:04:23 +0300

    Send mail JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,36 +57,93 @@ const calculateForm = document.getElementById('calculate-form'),
 const calculateBmi = (e) =>{
     e.preventDefault()
 
-// Check if the fields have a value
-if(calculateCm.value === '' || calculateKg.value === ''){
-    // Add and remove color
-    calculateMessage.classList.remove('color-green')
-    calculateMessage.classList.add('color-red')
+    // Check if the fields have a value
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        // Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
 
-    // Show message
-    calculateMessage.textContent = 'Fill in the Height and Weight ⌨️'
+        // Show message
+        calculateMessage.textContent = 'Fill in the Height and Weight '
 
-    // Remove message three seconds
-    setTimeout(() =>{
-        calculateMessage.textContent = '' 
-    }, 3000)
-} else{
-    // BMI Formula
-    const cm = calculateCm.value / 100,
-          kg = calculateKg.value,
-          bmi = Math.round(kg / (cm * cm))
+        // Remove message three seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = '' 
+        }, 3000)
+    } else{
+        // BMI Formula
+        const cm = calculateCm.value / 100,
+              kg = calculateKg.value,
+              bmi = Math.round(kg / (cm * cm))
 
-    // Show your health status
-    if(bmi < 18.5){
-        // Add color and display message
-        calculateMessage.classList.add('color-green')
-        calculateMessage.textContent = 'Your BMI is ${bmi} and you are skinny 😔'
+        // Show your health status
+        if(bmi < 18.5){
+            // Add color and display message
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are skinny 😔'
+        } else if(bmi < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are healthy 😉'
+        } else{
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ${bmi} and you are overweight 😉'
+        }
+        
+        // To clear the input field
+        calculateCm.value = ''
+        calculateKg.value = ''
+        
+        // Remove message four seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 4000)
     }
-
-}
 }
 
 calculateForm.addEventListener('submit', calculateBmi)
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message'),
+      contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // Check if the field has a value
+    if(contactUser.value === ''){
+        // Add and remove color
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        // Show message
+        contactMessage.textContent = 'You must enter your email 👆'
+
+        // Remove message after three seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 3000)
+    } else{
+        // ServiceID - templateID - #form - publicKey
+        emailjs.sendForm('service_tyurc4s','template_uruk5b1','#contact-form','DdRVQZaRvzhzjG25o')
+            .then(() =>{
+                // Show message and add color
+                contactMessage.classList.add('color-green')
+                contactMessage.textContent = 'You registered successfully 💪'
+
+                // Remove message after three seconds
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                }, 3000)
+            }, (error) => {
+                // Mail sending error
+                alert('OOPS! SOMETHING HAS FAILED...', error)
+            })
+
+        // To clear the input field
+        contactUser.value = ''        
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
